f269f85fce797da7e8adada48a53b7a72917d1b4
LJBang <kailo123457@gmail.com>
2019-08-30 14:11:47 +0900

    first

---
+++ app/src/main/java/com/example/desi/WeatherActivity.java
@@ -0,0 +1,15 @@
+package com.example.desi;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class WeatherActivity extends Activity {
+    @Override
+    protected  void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_layout);
+    }
+
+}
